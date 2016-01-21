b5c05c4931d34cdc4eebb3bf8005811b87113d24
Michael Mirtenbaum <mike.mirten@ocoa.com>
2016-01-21 14:34:38 -0500

    Navigation added to admin page

---
+++ src/App/AdminBundle/EventListener/HeaderNavigationListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\AdminBundle\EventListener;
+
+use App\UiBundle\Event\NavigationPrepareEvent;
+use App\UiBundle\Navigation\Button;
+
+class HeaderNavigationListener
+{
+	public function onHeadNavigation(NavigationPrepareEvent $event)
+	{
+		$btn = new Button('off');
+		$btn->setIcon('off');
+		
+		$event->getNavigation()->addElement($btn);
+	}
+	
+	public function onSideNavigation(NavigationPrepareEvent $event)
+	{
+		$content = new Button('content');
+		$content->setTitle('Content');
+		$content->setIcon('hdd');
+		
+		$pages = new Button('pages');
+		$pages->setTitle('Pages');
+		$pages->setIcon('file');
+		$pages->setRoute('app.admin');
+		
+		$content->addElement($pages);
+		
+		$elements = new Button('elements');
+		$elements->setTitle('Elements');
+		$elements->setIcon('list-alt');
+		
+		$content->addElement($elements);
+		
+		$event->getNavigation()->addElement($content);
+	}
+}
