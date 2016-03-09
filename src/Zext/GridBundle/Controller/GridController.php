0ec19ab7dc4e119aa272c9f12e406bc5d83e1551
Michael Mirtenbaum <mike.mirten@ocoa.com>
2016-03-09 11:33:01 -0500

    Default templates added

--- src/Zext/GridBundle/Controller/GridController.php
+++ src/Zext/GridBundle/Controller/GridController.php
@@ -7,21 +7,22 @@ use Symfony\Component\HttpFoundation\Request;
 
 class GridController extends Controller
 {
-	public function gridAction()
+	public function gridAction(Request $request)
 	{
-		$repo = $this->getDoctrine()->getRepository('AppCoreBundle:Page');
+		$repo = $this->getDoctrine()->getRepository('AppCoreBundle:PageElementPlacement');
 		
 		$reader = $this->get('annotation_reader');
 		$access = $this->get('property_accessor');
 		
 		$prvd = new \Zext\GridBundle\SchemaProvider\EntityAnnotationSchemaProvider($repo, $reader);
-		$proc = new \Zext\GridBundle\DataProcessor\EntityDataProcessor($access, $prvd);
+		$proc = new \Zext\GridBundle\DataProcessor\PropertyAccessDataProcessor($access, $prvd);
 		$src  = new \Zext\GridBundle\Source\RepositorySource($repo, $prvd, $proc);
 		
 		$grid = new \Zext\GridBundle\Grid\Grid($src);
 		
-		return $this->render('ZextGridBundle:Grid:grid.html.twig', [
-			'grid' => $grid
+		return $this->render('ZextGridBundle:Grid:page.html.twig', [
+			'isAjax' => $request->isXmlHttpRequest(),
+			'grid'   => $grid
 		]);
 	}
 }
